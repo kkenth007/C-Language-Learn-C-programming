8aa27a54f61d5b5400860b0b10bfcfbb78889495
Kanit Wijitpanya <32562129+kkenth007@users.noreply.github.com>
2017-11-02 19:34:06 +0700

    Update 5.2.c

--- 5.2.c
+++ 5.2.c
@@ -2,11 +2,9 @@
 int main()
 {
     int i, space, rows, k=0;
-
     printf("Enter number of rows: ");
     scanf("%d",&rows);
     printf("\n");
-
     for(i=1; i<=rows; ++i, k=0)
     {
         for(space=1; space<=rows-i; ++space)
@@ -37,15 +35,9 @@ int main()
             printf("*");
             P++;
         }
-
         printf("\n");
     }
     printf("\n");
-    
-    
-    
-    
-    
     //form 3
     int A, B, C;
     printf("Enter number of rows: ");
